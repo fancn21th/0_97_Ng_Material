0f2668316b72b8d27f51fcbf0c326d577ea6c6b7
fancn <fanyj@yikeer.com>
2019-09-13 10:35:57 +0800

    re-create: first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [BrowserAnimationsModule, MatToolbarModule]
+})
+export class MaterialModule {}
