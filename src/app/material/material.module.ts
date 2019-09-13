f046ea19eafdb788799f3d4754f891a07a0cfe91
fancn <fanyj@yikeer.com>
2019-09-13 10:48:11 +0800

    fix: css import issue

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatToolbarModule } from "@angular/material";
 
 @NgModule({
   declarations: [],
