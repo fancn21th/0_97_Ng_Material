5ad2e6de1cac2114d94fc89db5adcc7ae15440dc
fancn <fanyj@yikeer.com>
2019-09-13 11:03:02 +0800

    feature: sidenav layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { pluck } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ng-material';
+  public isSmallScreen: boolean;
+
+  constructor(private _breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this._breakpointObserver
+      .observe(["(max-width: 901px)"])
+      .pipe(pluck("matches"))
+      .subscribe((m: boolean) => (this.isSmallScreen = m));
+  }
+
+  get sidenavMode() {
+    return this.isSmallScreen ? "over" : "side";
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule } from "@angular/material";
-
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { LayoutModule } from "@angular/cdk/layout";
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: [BrowserAnimationsModule, MatToolbarModule]
+  imports: [],
+  exports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    LayoutModule
+  ]
 })
 export class MaterialModule {}
